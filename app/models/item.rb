0912b73b20ac296926836e43e3fe81a2fd25d687
hiroto.takeuchi <takehiro199@icloud.com>
2019-03-10 07:38:21 +0000

    want

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
-    validates :code, precense: true, length: { maximum: 225 }
-    validates :name, precense: true, length: { maximum: 225 }
-    validates :url, precense: true, length: { maximum: 225 }
-    validates :image_url, precense: true, length: { maximum: 225 }
+    validates :code, presence: true, length: { maximum: 225 }
+    validates :name, presence: true, length: { maximum: 225 }
+    validates :url, presence: true, length: { maximum: 225 }
+    validates :image_url, presence: true, length: { maximum: 225 }
+    
+    has_many :ownerships
+    has_many :users, through: :ownerships
+    has_many :wants
+    has_many :want_users, through: :ownerships, source: :user
 end
